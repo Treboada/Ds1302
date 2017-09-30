f5cd519df4ee0d83d6fb9dc8001a4bc7e6f1cf32
Rafa Couto <caligari@treboada.net>
2017-09-30 05:02:41 +0200

    First alpha version.

---
+++ src/Ds1302.cpp
@@ -0,0 +1,161 @@
+// Ds1302.cpp
+
+#include "Ds1302.h"
+
+#include <Arduino.h>
+
+#define REG_SECONDS           0x80
+#define REG_MINUTES           0x82
+#define REG_HOUR              0x84
+#define REG_DATE              0x86
+#define REG_MONTH             0x88
+#define REG_DAY               0x8A
+#define REG_YEAR              0x8C
+#define REG_BURST             0xBE
+
+
+Ds1302::Ds1302(uint8_t pin_ena, uint8_t pin_clk, uint8_t pin_dat)
+{
+    _pin_ena = pin_ena;
+    _pin_clk = pin_clk;
+    _pin_dat = pin_dat;
+    _dat_direction = INPUT;
+}
+
+
+void Ds1302::init()
+{
+    pinMode(_pin_ena, OUTPUT);
+    pinMode(_pin_clk, OUTPUT);
+    pinMode(_pin_dat, _dat_direction);
+
+    digitalWrite(_pin_ena, LOW);
+    digitalWrite(_pin_clk, LOW);
+}
+
+
+bool Ds1302::isHalted()
+{
+    _prepareRead(REG_SECONDS);
+    uint8_t seconds = _readByte();
+    return (seconds & 0b01000000);
+}
+
+
+void Ds1302::getDateTime(DateTime* dt)
+{
+    _prepareRead(REG_BURST);
+    dt->second = _bcd2dec(_readByte() & 0b01111111);
+    dt->minute = _bcd2dec(_readByte() & 0b01111111);
+    dt->hour   = _bcd2dec(_readByte() & 0b00111111);
+    dt->day    = _bcd2dec(_readByte() & 0b00111111);
+    dt->month  = _bcd2dec(_readByte() & 0b00011111);
+    dt->dow    = _bcd2dec(_readByte() & 0b00000111);
+    dt->year   = _bcd2dec(_readByte() & 0b01111111);
+}
+
+
+void Ds1302::setDateTime(DateTime* dt)
+{
+    _prepareWrite(REG_BURST);
+    _writeByte(_dec2bcd(dt->second % 60 ));
+    _writeByte(_dec2bcd(dt->minute % 60 ));
+    _writeByte(_dec2bcd(dt->hour   % 24 ));
+    _writeByte(_dec2bcd(dt->day    % 31 ));
+    _writeByte(_dec2bcd(dt->month  % 12 ));
+    _writeByte(_dec2bcd(dt->dow    % 7  ));
+    _writeByte(_dec2bcd(dt->year   % 100));
+}
+
+
+void Ds1302::halt()
+{
+    _prepareWrite(REG_SECONDS);
+    _writeByte(0b10000000);
+}
+
+
+void Ds1302::_prepareRead(uint8_t address)
+{
+    _setDirection(INPUT);
+    digitalWrite(_pin_ena, HIGH);
+    uint8_t command = 0b10000001 | address;
+    _writeByte(command);
+}
+
+
+void Ds1302::_prepareWrite(uint8_t address)
+{
+    _setDirection(OUTPUT);
+    digitalWrite(_pin_ena, HIGH);
+    uint8_t command = 0b10000000 | address;
+    _writeByte(command);
+}
+
+
+void Ds1302::_end()
+{
+    digitalWrite(_pin_ena, LOW);
+}
+
+
+uint8_t Ds1302::_readByte()
+{
+    uint8_t byte;
+
+    for(uint8_t b = 0; b < 8; b++)
+    {
+        byte <<= 1;
+
+        digitalWrite(_pin_clk, HIGH);
+        delayMicroseconds(1);
+
+        if (digitalRead(_pin_dat) == HIGH) byte |= 0x01;
+
+        digitalWrite(_pin_clk, LOW);
+        delayMicroseconds(1);
+    }
+
+    return byte;
+}
+
+
+void Ds1302::_writeByte(uint8_t value)
+{
+    for(uint8_t b = 0; b < 8; b++)
+    {
+        digitalWrite(_pin_clk, HIGH);
+        delayMicroseconds(1);
+
+        digitalWrite(_pin_dat, (value & 0x01) ? HIGH : LOW);
+
+        digitalWrite(_pin_clk, LOW);
+        delayMicroseconds(1);
+
+        value >>= 1;
+    }
+}
+
+
+void Ds1302::_setDirection(int direction)
+{
+    if (_dat_direction != direction)
+    {
+        _dat_direction = direction;
+        pinMode(_pin_dat, direction);
+    }
+}
+
+
+uint8_t Ds1302::_dec2bcd(uint8_t dec)
+{
+    return ((dec / 10 * 16) + (dec % 10));
+}
+
+
+uint8_t Ds1302::_bcd2dec(uint8_t bcd)
+{
+    return ((bcd / 16 * 10) + (bcd % 16));
+}
+
+
---
+++ src/Ds1302.h
@@ -0,0 +1,104 @@
+// Ds1302.h
+
+#ifndef _DS_1302_H
+#define _DS_1302_H
+
+#include <stdint.h>
+
+class Ds1302
+{
+    public:
+
+        typedef struct {
+            uint8_t year;
+            uint8_t month;
+            uint8_t day;
+            uint8_t hour;
+            uint8_t minute;
+            uint8_t second;
+            uint8_t dow;
+        } DateTime;
+
+        /**
+         * Months of year
+         */
+        enum MONTH : uint8_t {
+            MONTH_JAN = 1,
+            MONTH_FEB = 2,
+            MONTH_MAR = 3,
+            MONTH_APR = 4,
+            MONTH_MAY = 5,
+            MONTH_JUN = 6,
+            MONTH_JUL = 7,
+            MONTH_AUG = 8,
+            MONTH_SET = 9,
+            MONTH_OCT = 10,
+            MONTH_NOV = 11,
+            MONTH_DEC = 12
+        };
+
+        /**
+         * Days of week
+         */
+        enum DOW : uint8_t {
+            DOW_MON = 1,
+            DOW_TUE = 2,
+            DOW_WED = 3,
+            DOW_THR = 4,
+            DOW_FRI = 5,
+            DOW_SAT = 6,
+            DOW_SUN = 7
+        };
+
+        /**
+         * Constructor (pin configuration).
+         */
+        Ds1302(uint8_t pin_ena, uint8_t pin_clk, uint8_t pin_dat);
+
+        /**
+         * Initializes the DW1302 chip.
+         */
+        void init();
+
+        /**
+         * Returns when the oscillator is disabled.
+         */
+        bool isHalted();
+
+        /**
+         * Stops the oscillator.
+         */
+        void halt();
+
+        /**
+         * Returns the current date and time.
+         */
+        void getDateTime(DateTime* dt);;
+
+        /**
+         * Sets the current date and time.
+         */
+        void setDateTime(DateTime* dt);
+
+    private:
+
+        uint8_t _pin_ena;
+        uint8_t _pin_clk;
+        uint8_t _pin_dat;
+
+        void _prepareRead(uint8_t address);
+        void _prepareWrite(uint8_t address);
+        void _end();
+
+        int _dat_direction;
+        void _setDirection(int direction);
+
+        uint8_t _readByte();
+        void _writeByte(uint8_t value);
+
+        uint8_t _dec2bcd(uint8_t dec);
+        uint8_t _bcd2dec(uint8_t bcd);
+};
+
+#endif // _DS_1302_H
+
